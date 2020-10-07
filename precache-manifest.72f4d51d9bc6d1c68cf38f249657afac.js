292db7eed79616efdc88075972d01faaa76802bb
javalosjr96 <javalosjr96@gmail.com>
2020-10-07 15:52:29 -0700

    Updates

---
+++ precache-manifest.72f4d51d9bc6d1c68cf38f249657afac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194d8a93ad491141b729c7421ee1fb06",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "82313854958d52c53d7f",
+    "url": "/ToDoList/static/css/main.ff8345ff.chunk.css"
+  },
+  {
+    "revision": "cded9f770746bde75ae9",
+    "url": "/ToDoList/static/js/2.f4991ccb.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/ToDoList/static/js/2.f4991ccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82313854958d52c53d7f",
+    "url": "/ToDoList/static/js/main.96691f3b.chunk.js"
+  },
+  {
+    "revision": "e274c1a13bd7704e2eb6",
+    "url": "/ToDoList/static/js/runtime-main.d8fdeb38.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ToDoList/static/media/logo.5d5d9eef.svg"
+  }
+]);
